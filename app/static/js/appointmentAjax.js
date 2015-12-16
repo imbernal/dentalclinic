89890088585a5195b69674ffaf53cb29c5c1b44b
Israel Bernal Martinez <imbernal9203@gmail.com>
2015-12-16 00:55:07 -0800

    Big Commit about scripts that i dont use

--- app/static/js/appointmentAjax.js
+++ app/static/js/appointmentAjax.js
@@ -1,30 +1,74 @@
-$('#submitAppointment').click(function (e) {
-    //debugger;
-    e.preventDefault();
-
-
-    var urlPost = $('#url').val();
-    $.ajax({
-        method: "POST",
-        url: urlPost,
-        data: {
-            name: $('#name').val(),
-            email: $('#email').val(),
-            phone: $('#phone').val(),
-            date: $('#date').val(),
-            hour: $('#clock').val()
+$('#formAppointment').validate({
+    rules: {
+        name: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        phone: {
+            required: true
+        },
+        date: {
+            required: true
+        },
+        hour: {
+            required: true
         }
-    })
-        .done(function () {
-                $('#name').val(""),
-                $('#email').val(""),
-                $('#phone').val(""),
-                $('#date').val(""),
-                $('#clock').val("")
-        });
+    },
+    submitHandler: function (form) {
 
-
+        $.ajax({
+            method: "POST",
+            url: '/appointment/',
+            data: {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                phone: $('#phone').val(),
+                date: $('#date').val(),
+                hour: $('#clock').val()
+            }
+        })
+            .done(function () {
+                $('#name').val("");
+                $('#email').val("");
+                $('#phone').val("");
+                $('#date').val("");
+                $('#clock').val("");
+                $('#myModal').modal('show');
+            });
+    }
 });
+
+//
+//$('#submitAppointment').click(function (e) {
+//    //debugger;
+//    e.preventDefault();
+//
+//
+//    var urlPost = $('#url').val();
+//    $.ajax({
+//        method: "POST",
+//        url: urlPost,
+//        data: {
+//            name: $('#name').val(),
+//            email: $('#email').val(),
+//            phone: $('#phone').val(),
+//            date: $('#date').val(),
+//            hour: $('#clock').val()
+//        }
+//    })
+//        .done(function () {
+//                $('#name').val(""),
+//                $('#email').val(""),
+//                $('#phone').val(""),
+//                $('#date').val(""),
+//                $('#clock').val("")
+//        });
+//
+//
+//});
 
 $('#contactSubmit').click(function (e) {
     //debugger;
@@ -43,10 +87,10 @@ $('#contactSubmit').click(function (e) {
         }
     })
         .done(function () {
-                $('#apname').val("");
-                $('#apemail').val("");
-                $('#aptel').val("");
-                $('#apcomment').val("");
+            $('#apname').val("");
+            $('#apemail').val("");
+            $('#aptel').val("");
+            $('#apcomment').val("");
         });
 
 
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -83,51 +83,6 @@ $(document).ready(function () {
     });
 });
 
-/*checker */
-
-
-
-/*$(document).ready(function () {
-
-    $('div[id^="down"]').each(function (i, obj) {
-        $(this).hide();
-        $(this).css('background-color', '#F0F8FF');
-        $(this).css('padding', '28px 15px');
-    });
-
-    $("#down1").slideDown("slow");
-    $("#top1 h3").addClass('minus');
-    $("#top1 h3").css('background-image', 'url(images/icons/flip-ico.png)');
-	
-    $('h3[id^="disc"]').click(function () {
-        var CurrElemId = $(this).attr("id");
-        var index = CurrElemId.substr(CurrElemId.length - 1);
-        var totalnumber = $("li[id^='top']").length;
-
-        for (var i = 1; i <= totalnumber; i++) {
-            var temp = "disc" + i;
-            if (CurrElemId == temp) {
-                $("#top" + i).prependTo("#TopLevelList");
-                if ($("#down" + i).is(":visible")) {
-                    $("#top" + i + " h3").removeClass('minus');
-                    $("#top" + i + " h3").css('background-image', 'url(images/icons/flip-ico+.png)');
-                    $("#down" + i).slideUp("slow");
-                }
-                else {
-                    $("#top" + i + " h3").addClass('minus');
-                    $("#top" + i + " h3").css('background-image', 'url(images/icons/flip-ico.png)');
-                    $("#down" + i).slideDown("slow");
-                }
-            }
-            else {
-                $("#top" + i + " h3").removeClass('minus');
-                $("#top" + i + " h3").css('background-image', 'url(images/icons/flip-ico+.png)');
-                $("#down" + i).slideUp("slow");
-            }
-        }
-    });
-});
-*/
 
 $(document).ready(function () {
 	$('div[id^="down"]').each(function (i, obj) {
