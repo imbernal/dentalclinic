b881e5b96485914548de016def3156fb635d8abd
Israel Michel Bernal Martinez <imbernal@israels-mbp.attlocal.net>
2016-01-21 13:12:21 -0500

    Email changes

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -76,7 +75,7 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 EMAIL_HOST = "smtp.mandrillapp.com"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = "imbernal9203@gmail.com"
-EMAIL_HOST_PASSWORD = "@1992.@Isra@"
+EMAIL_HOST_PASSWORD = "b_Aio-LIxDdptnjOtSVVSw"
 
 # EMAIL_USE_TLS = True
 # EMAIL_HOST = "smtp.gmail.com"
